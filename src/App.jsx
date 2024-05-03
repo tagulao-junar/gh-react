38ff8c469123ef9c1e31c38a01c63299ec5042de
Junar Tagulao <junar.tagulao@gmail.com>
2024-05-03 18:16:30 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Destination from './components/Destination'
+
+
+const App = () => {
+  return (
+    <div>
+       <Navbar/>
+       <Banner/>
+       <Destination/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar Here Navbar Here Navbar Here Navbar Here</div>
+  )
+}
+
+export default Navbar
+
+
+
+
+
